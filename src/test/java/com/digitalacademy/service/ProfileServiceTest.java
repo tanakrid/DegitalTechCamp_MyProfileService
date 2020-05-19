f23c747da878f257e849e187600027609a8cd4a5
tanakrid chanburi <tanakrid.c@ku.th>
2020-05-19 23:05:52 +0700

    add test ProfileService

--- src/test/java/com/digitalacademy/service/ProfileServiceTest.java
+++ src/test/java/com/digitalacademy/service/ProfileServiceTest.java
@@ -1,5 +1,8 @@
 package com.digitalacademy.service;
 
+import com.digitalacademy.constants.ProfileError;
+import com.digitalacademy.exception.ProfileException;
+import com.digitalacademy.model.Profile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -25,16 +27,44 @@ public class ProfileServiceTest {
         profileService = new ProfileService();
     }
 
-//    @DisplayName("Test get loan by in equals 2 should throw Loan Exception loan info not found")
-//    @Test
-//    void testGetLoanInfoByIdEquals2() throws Exception{
-//        Long reqParam = 2L;
-//        LoanException thrown = assertThrows(LoanException.class,
-//                () -> loanService.getLoanInfoById(reqParam),
-//                "Expected LoanInfoById(reqParam) to throw, but it didn't");
-//
-//        assertEquals(400, thrown.getHttpStatus().value());
-//        assertEquals("LOAN4002", thrown.getLoanError().getCode());
-//        assertEquals("Loan information not found", thrown.getLoanError().getMessage());
-//    }
+    @DisplayName("Test get profile info by id equals 7 should return profile information")
+    @Test
+    public void testProfileInfoByIdEquals7() throws Exception{
+        Profile resp = profileService.getProfile(7L);
+        assertEquals("7", resp.getId().toString());
+        assertEquals("Tanakrid", resp.getFirstName());
+        assertEquals("Chanburi", resp.getLastName());
+        assertEquals(21, resp.getAge());
+        assertEquals("0837704301", resp.getTelephone());
+        assertEquals("Kasetsart University",resp.getGraduatedForm());
+        assertEquals(2.98,resp.getGrade());
+        assertEquals("Computer Science",resp.getMajor());
+        assertEquals("Software Engineer",resp.getJob());
+    }
+
+    @DisplayName("Test get profile info by id equals 1 should return profile information")
+    @Test
+    public void testProfileInfoByIdEquals1() throws Exception{
+        Profile resp = profileService.getProfile(1L);
+        assertEquals("1", resp.getId().toString());
+        assertNull(resp.getFirstName());
+        assertNull(resp.getLastName());
+        assertEquals(0, resp.getAge());
+        assertNull(resp.getTelephone());
+        assertNull(resp.getGraduatedForm());
+        assertEquals(0, resp.getGrade());
+        assertNull(resp.getMajor());
+        assertNull(resp.getJob());
+    }
+
+    @DisplayName("Test get profile by excepted id 7 and 1 should throw Loan Exception loan info not found")
+    @Test
+    void testGetLoanInfoByIdEqualsOther() throws Exception{
+        Long reqParam = 10L;
+        ProfileException thrown = assertThrows(ProfileException.class
+                , () -> profileService.getProfile(reqParam));
+        assertEquals(400, thrown.getHttpStatus().value());
+        assertEquals("PROFILE4002", thrown.getProfileError().getCode());
+        assertEquals("profile information not found", thrown.getProfileError().getMessage());
+    }
 }
